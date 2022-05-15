e8429607ff9c03c02569055bfcab68a61a199780
vagpinto <vagpinto@sga.pucminas.br>
2022-05-15 11:51:25 -0300

    Ajustes de conflito

--- src/js/models/user.js
+++ src/js/models/user.js
@@ -4,22 +4,15 @@
  */
 
 export class User {
-<<<<<<< HEAD
-  constructor(name, email, password, created_at) {
-=======
   constructor(name, email, password) {
->>>>>>> f27baf3ba608bba6c496bdcb07d4ae9498509896
     this.id = undefined;
     this.name = name;
     this.email = email;
     this.password = password;
-<<<<<<< HEAD
-=======
     this.objective = null;
     this.participated_teams = null;
     this.contact_info = null;
     this.game_statistics = null;
     this.game_roles = null;
->>>>>>> f27baf3ba608bba6c496bdcb07d4ae9498509896
   }
 }
